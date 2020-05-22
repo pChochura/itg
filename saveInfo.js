078e9d085daf636071d6b5b6df087276028cc6ba
Paweł Chochura <pchochura@gmail.com>
2020-05-22 15:03:47 +0200

    prettier

--- saveInfo.js
+++ saveInfo.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
 
 const packageJson = require('./package.json');
-fs.writeFileSync('./info.json', JSON.stringify({ version: packageJson.version }));
+fs.writeFileSync(
+	'./info.json',
+	JSON.stringify({ version: packageJson.version }),
+);
